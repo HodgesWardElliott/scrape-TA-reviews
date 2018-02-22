ccecb66866d582e24e4d30ae210faa430607ca0a
timkiely <timothy.j.kiely@gmail.com>
2018-02-22 17:37:28 -0500

    added parallel execution to html processing

--- analyze-hotel-data.R
+++ analyze-hotel-data.R
@@ -9,23 +9,38 @@ total_list <- read_rds("data/processed_html_data.rds")
 
 hotel_data <- 
   total_list %>% 
+  map(~.[[1]]) %>% 
   map(~ifelse(lengths(.x)!=1, NA, .x)) %>% 
   map(bind_rows) %>% 
-  bind_rows()
+  bind_rows() %>% 
+  mutate(City = str_replace_all(address_city, "[, ]|[A-Z][A-Z]|[0-9]+[-][0-9]+|[0-9]+",""))
 
 # count of hotels
 hotel_data %>% 
-  mutate(City = str_replace_all(address_city, "[, ]|[A-Z][A-Z]|[0-9]+[-][0-9]+|[0-9]+","")) %>% 
   group_by(City) %>% 
   tally()
 
-hotel_data <- hotel_data %>% mutate(City = str_replace_all(address_city, "[, ]|[A-Z][A-Z]|[0-9]+[-][0-9]+|[0-9]+",""))
 
+
+# Geo-coded via carto -----------------------------------------------------
 # write_csv(hotel_data, 'data/export-to-carto.csv')
-
 hotels_sf <- read_sf("data/import_from_carto.geojson")
 
-atl <- hotels_sf %>% filter(city=="Atlanta")
+hotels_sf <- 
+  hotels_sf %>% 
+  mutate_at(vars(title:terrible_count), function(x) ifelse(x=="NA",0,x)) %>% 
+  mutate_at(vars(count_main_photos:terrible_count), function(x) ifelse(x=="NA",0,x)) %>% 
+  mutate_at(vars(count_main_photos:terrible_count), as.numeric)
 
-atl %>% 
+
+write_sf(hotels_sf, "data/TA-review-data.geojson")
   
+
+library(spdep)
+nb <- dnearneigh(st_coordinates(hotels_sf), 0, 500, na.)
+
+
+
+
+
+
--- extract-hotel-data.R
+++ extract-hotel-data.R
@@ -1,15 +1,17 @@
 
+run_start <- Sys.time()
 # load packages:
 if(!"pacman"%in%installed.packages()){
   install.packages("pacman")
 }
-pacman::p_load(rvest, tidyverse, stringr, httr, parallel)
+pacman::p_load(rvest, tidyverse, stringr, httr, parallel, foreach, doParallel)
 
-# flow control for file existenceL:
+# flow control for file existence:
 if(!file.exists("data/OUTPUT-hotel-link-list.csv")) stop("File require: OUTPUT-hotel-link-list.csv. Did you run scrape-tripadvisor-data.R yet?")
 all_links <- suppressMessages(read_csv("data/OUTPUT-hotel-link-list.csv"))
 if(length(dir('data/html_raw'))!=length(unique(all_links$href))) stop("You seem to be missing some links int he html_raw folder")
 
+# "https://www.tripadvisor.com/Hotel_Review-g60898-d114380-Reviews-JW_Marriott_Atlanta_Buckhead-Atlanta_Georgia.html"
 
 # cheeck to see if some of the pages have already been processed:
 all_pages <- dir('data/html_raw')
@@ -20,126 +22,136 @@ if(file.exists("data/raw_html_already_processed.csv")) {
 }
 yet_to_process <- all_pages[!all_pages %in% unique(already_processed$pages_of_interest)]
 
-# extract data from the html files:
+cl <- makeCluster(parallel::detectCores()-2)
+registerDoParallel(cl)
+
 total_list <- list()
-for(jj in 1:length(yet_to_process)){
-  # jj <- 1
-  page_of_interest <- yet_to_process[jj]
-  message("Working on ",jj," of ",length(yet_to_process))
-  
-  raw_html <- read_html(paste0('data/html_raw/',page_of_interest))
-  
-  # extract data ------------------------------------------------------------
-  
-  data_list <- list()
-  
-  data_list$title <- 
-    raw_html %>% 
-    html_nodes("#HEADING") %>% 
-    html_text()
-  
-  data_list$rating_of_five <- 
-    raw_html %>% 
-    html_nodes("#taplc_resp_hr_atf_hotel_info_0 .bubble_45") %>% 
-    html_attr("alt") %>% 
-    str_replace_all(" of 5 bubbles","") %>% 
-    as.numeric()
-  
-  data_list$number_of_reviews <- 
-    raw_html %>% 
-    html_nodes(".reviewCount") %>% 
-    html_text() %>% 
-    str_replace_all(" reviews","") %>% 
-    as.numeric()
-  
-  data_list$Rank_in_city <- 
-    raw_html %>% 
-    html_nodes(".rank") %>% 
-    html_text() %>% 
-    str_replace_all("[#]","") %>% 
-    as.numeric()
-  
-  data_list$total_in_rank_list <- 
-    raw_html %>% 
-    html_nodes(".popIndexValidation") %>% 
-    html_text() %>% 
-    str_replace_all("[#][0-9]+ Best Value of | [hH]otels in *.*|[#][0-9]+ of ","") %>% 
-    as.numeric()
-  
-  address_street <- 
-    raw_html %>% 
-    html_node(".street-address") %>% 
-    html_text()
-  
-  data_list$address_street <- address_street
-  
-  address_city <- 
-    raw_html %>% 
-    html_node(".locality") %>% 
-    html_text()
-  
-  data_list$address_city <- address_city
-  
-  data_list$full_address <- paste(address_street, address_city, sep = ", ")
-  
-  data_list$cert_of_excellence <- 
-    raw_html %>% 
-    html_node(".certificate-of-excellence") %>% 
-    html_text()
-  
-  data_list$count_main_photos <- 
-    raw_html %>% 
-    html_node(".see_all_count .is-hidden-mobile") %>% 
-    html_text() %>% 
-    str_replace_all(" All photos [(]|[)]","") %>% 
-    as.numeric()
-  
-  data_list$overall_rating <- 
-    raw_html %>% 
-    html_node(".overallRating") %>% 
-    html_text() %>% 
-    str_replace_all(" ","") %>% 
-    as.numeric()
-  
-  
-  data_list$count_of_reviews <- 
-    raw_html %>% 
-    html_node(".reviews_header_count") %>% 
-    html_text() %>% 
-    str_replace_all("[()]","") %>% 
-    as.numeric()
-  
-  
-  sub_ratings <- 
-    raw_html %>% 
-    html_node(".is-5 .choices") %>% 
-    html_text()
-  
-  sub_choices <- str_split(sub_ratings, "[0-9]+") %>% .[[1]] %>% .[.!=""]
-  sub_counts <- str_replace_all(sub_ratings, "Excellent|Very good|Average|Poor|Terrible| "," ") %>% str_split(" ") %>% .[[1]] %>% .[.!=""]
-  
-  data_list$Excellet_count <- as.numeric(sub_counts[1])
-  data_list$VGood_count <- as.numeric(sub_counts[2])
-  data_list$Average_count <- as.numeric(sub_counts[3])
-  data_list$Poor_count <- as.numeric(sub_counts[4])
-  data_list$Terrible_count <- as.numeric(sub_counts[5])
-  
-  total_list[[data_list$title]] <- data_list
-  
-  if(file.exists("data/raw_html_already_processed.csv")){
-    write_csv(data_frame("pages_of_interest" = page_of_interest), "data/raw_html_already_processed.csv", append = TRUE)
+total_list <- 
+  foreach(jj = 1:length(yet_to_process)
+  , .packages = c('rvest', 'tidyverse', 'stringr', 'httr')) %dopar% {
+    #for(jj in 1:length(yet_to_process)){
+    # jj <- 1
+    page_of_interest <- yet_to_process[jj]
+    message("Working on ",jj," of ",length(yet_to_process))
+    
+    raw_html <- read_html(paste0('data/html_raw/',page_of_interest))
+    
+    # extract data ------------------------------------------------------------
+    
+    data_list <- list()
+    
+    data_list$title <- 
+      raw_html %>% 
+      html_nodes("#HEADING") %>% 
+      html_text()
+    
+    data_list$rating_of_five <- 
+      raw_html %>% 
+      html_nodes("#taplc_resp_hr_atf_hotel_info_0 .bubble_45") %>% 
+      html_attr("alt") %>% 
+      str_replace_all(" of 5 bubbles","") %>% 
+      as.numeric()
+    
+    data_list$number_of_reviews <- 
+      raw_html %>% 
+      html_nodes(".reviewCount") %>% 
+      html_text() %>% 
+      str_replace_all(" reviews","") %>% 
+      as.numeric()
+    
+    data_list$Rank_in_city <- 
+      raw_html %>% 
+      html_nodes(".rank") %>% 
+      html_text() %>% 
+      str_replace_all("[#]","") %>% 
+      as.numeric()
+    
+    data_list$total_in_rank_list <- 
+      raw_html %>% 
+      html_nodes(".popIndexValidation") %>% 
+      html_text() %>% 
+      str_replace_all("[#][0-9]+ Best Value of | [hH]otels in *.*|[#][0-9]+ of ","") %>% 
+      as.numeric()
+    
+    address_street <- 
+      raw_html %>% 
+      html_node(".street-address") %>% 
+      html_text()
+    
+    data_list$address_street <- address_street
+    
+    address_city <- 
+      raw_html %>% 
+      html_node(".locality") %>% 
+      html_text()
+    
+    data_list$address_city <- address_city
+    
+    data_list$full_address <- paste(address_street, address_city, sep = ", ")
+    
+    data_list$cert_of_excellence <- 
+      raw_html %>% 
+      html_node(".certificate-of-excellence") %>% 
+      html_text()
+    
+    data_list$count_main_photos <- 
+      raw_html %>% 
+      html_node(".see_all_count .is-hidden-mobile") %>% 
+      html_text() %>% 
+      str_replace_all(" All photos [(]|[)]","") %>% 
+      as.numeric()
+    
+    data_list$overall_rating <- 
+      raw_html %>% 
+      html_node(".overallRating") %>% 
+      html_text() %>% 
+      str_replace_all(" ","") %>% 
+      as.numeric()
+    
+    
+    data_list$count_of_reviews <- 
+      raw_html %>% 
+      html_node(".reviews_header_count") %>% 
+      html_text() %>% 
+      str_replace_all("[()]","") %>% 
+      as.numeric()
+    
+    
+    sub_ratings <- 
+      raw_html %>% 
+      html_node(".is-5 .choices") %>% 
+      html_text()
+    
+    sub_choices <- str_split(sub_ratings, "[0-9]+") %>% .[[1]] %>% .[.!=""]
+    sub_counts <- str_replace_all(sub_ratings, "Excellent|Very good|Average|Poor|Terrible| "," ") %>% str_split(" ") %>% .[[1]] %>% .[.!=""]
+    
+    data_list$Excellet_count <- as.numeric(sub_counts[1])
+    data_list$VGood_count <- as.numeric(sub_counts[2])
+    data_list$Average_count <- as.numeric(sub_counts[3])
+    data_list$Poor_count <- as.numeric(sub_counts[4])
+    data_list$Terrible_count <- as.numeric(sub_counts[5])
+    
+    data_list$full_link <- paste0("https://www.tripadvisor.com/",page_of_interest)
+    
+    data_list <- list(data_list)
+    names(data_list) <- data_list[[1]]$title
+    
+    if(file.exists("data/raw_html_already_processed.csv")){
+      write_csv(data_frame("pages_of_interest" = page_of_interest), "data/raw_html_already_processed.csv", append = TRUE)
     } else {
       write_csv(data_frame("pages_of_interest" = page_of_interest), "data/raw_html_already_processed.csv", append = FALSE)
-      }
-  write_rds(total_list, "data/processed_html_data.rds")
-  }
+    }
+    return(data_list)
+  }; stopCluster(cl)
 
+write_rds(total_list, "data/processed_html_data.rds")
 
 # file.remove("data/raw_html_already_processed.csv")
 # file.remove("data/processed_html_data.rds")
 
-
-
+run_end <- Sys.time()
+tot_time <- run_end - run_start # 1.5 mins on 10 cores
+tot_time
 
 
 
